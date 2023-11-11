8012837f1deafa9824f556898d80a31f498e4111
cbct <clongnguynvn@gmail.com>
2023-11-11 09:59:18 +0700

    completed Lab4

--- SOURCECODE/LAB4/Core/Src/fsm_uart.c
+++ SOURCECODE/LAB4/Core/Src/fsm_uart.c
@@ -27,7 +27,7 @@ uint8_t cmd_flag = 0;// 1=Got a command, 0=Nothing
 char cmd_data [MAX_BUFFER_SIZE]= "";//storing command, exclude "!" and "#" symbols
 uint8_t idx_cmd_data=0;//current index of char cmd_data
 
-uint8_t strr[30];//array to store output string
+uint8_t strr[30];//array to store b output string
 uint32_t ADC_value;
 
 void command_parser_fsm (){
@@ -43,14 +43,14 @@ void command_parser_fsm (){
 	case CMD_BODY://scan for a command or a string to store
 		if(buffer[curr_idx_buffer]=='!') break;
 		if(buffer[curr_idx_buffer]=='#') {
-			cmd_data[idx_cmd_data] = '\0';
-			if(strcmp(cmd_data, "RST") == 0) {
-				cmd_flag = 1;
-			}
-			else if(strcmp(cmd_data,"OK")  == 0) {
-				cmd_flag = 1;
-			}
 			cmd_state = CMD_INIT;
+			if(compare(cmd_data, "RST",3) == 1){
+				cmd_flag = 1;
+			}
+			if(compare(cmd_data,"OK",2)  == 1){
+				cmd_flag=2;
+			}
+			clear_cmd_data();
 		}
 		else {
 			cmd_data[idx_cmd_data++] = buffer[curr_idx_buffer];
@@ -63,26 +63,48 @@ void command_parser_fsm (){
 void uart_communiation_fsm () {
 	switch (uart_state){
 	case UART_INIT:
-		if(cmd_flag==1 && strcmp(cmd_data, "RST") == 0) {
+		if(cmd_flag==1) {//RST
 			cmd_flag = 0;
 			uart_state = UART_WAIT;
-			HAL_GPIO_TogglePin (LED_YELLOW_GPIO_Port, LED_YELLOW_Pin ) ;
-			ADC_value = HAL_ADC_GetValue (&hadc1);
-			int len = sprintf ( strr , "\r\n!ADC=%d#\r\n", ADC_value );
-			HAL_UART_Transmit (&huart2 , (void *) strr, len, 1000);
-			setTimer(0,300);
+			HAL_GPIO_TogglePin (LED_YELLOW_GPIO_Port, LED_YELLOW_Pin);
+			printADC();//with setTimer 3s
 		}
 		break;
 	case UART_WAIT:
-		if(cmd_flag==1 && strcmp(cmd_data, "OK") == 0) {
+		if(cmd_flag==2) {//OK
 			cmd_flag = 0;
 			uart_state = UART_INIT;
+			HAL_GPIO_TogglePin (LED_YELLOW_GPIO_Port, LED_YELLOW_Pin);
+			nextLine();
 		}
-		else if (timer_flag[0]==1){
-			int len = sprintf ( strr , "\r\n!ADC=%d#\r\n", ADC_value );
-			HAL_UART_Transmit (&huart2 , (void *) strr, len, 1000);
-			setTimer(0,300);
+		else if (timer_flag[0]==1){//3s timer
+			printADC();//with setTimer 3s
 		}
 		break;
 	}
+}
+void printADC(){
+	HAL_ADC_Start(&hadc1);
+	ADC_value = HAL_ADC_GetValue (&hadc1);
+	HAL_ADC_Stop(&hadc1);
+	int len = sprintf ( strr , "\r\n!ADC=%d#\r\n", ADC_value );
+	HAL_UART_Transmit (&huart2 , (void *) strr, len, 1000);
+	setTimer(0,300);
+}
+void nextLine(){
+	HAL_UART_Transmit (&huart2 , "\r", 1, 1000);
+}
+void clear_cmd_data(){
+	for(int i=0;i<MAX_BUFFER_SIZE;i++){
+		cmd_data[i]="\0";
+	}
+	idx_cmd_data=0;
+}
+int compare(char cmd_data[],char str[],int str_size){
+	for(int i=0;i<str_size;i++){
+		if(cmd_data[i]!=str[i]){
+			return 0;
+		}
+	}
+	return 1;
 }
