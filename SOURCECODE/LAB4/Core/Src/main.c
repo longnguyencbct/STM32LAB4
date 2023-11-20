b7bdbd7d4562ef956c4090f8a0735a2e426b57be
cbct <clongnguynvn@gmail.com>
2023-11-20 13:58:51 +0700

    temp

--- SOURCECODE/LAB4/Core/Src/main.c
+++ SOURCECODE/LAB4/Core/Src/main.c
@@ -72,7 +72,7 @@ uint8_t buffer_flag = 0;
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 	if(huart->Instance == USART2){
 
-		//HAL_UART_Transmit(&huart2, &temp, 1, 50);
+		HAL_UART_Transmit(&huart2, &temp, 1, 50);
 		buffer[index_buffer++] = temp;
 		if(index_buffer == 30) index_buffer = 0;
 
